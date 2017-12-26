32f045ca76ffc761bb4fe3eab501e39c752b1100
Samuzzal Choudhury <samuzzal@redhat.com>
2017-12-26 17:26:21 +0530

    clean repo for gunicorn based api server

--- src/schemas/__init__.py
+++
@@ -1,5 +0,0 @@
-from f8a_worker.schemas import BundledDynamicSchemaLibrary
-
-_server_schemas_lib = BundledDynamicSchemaLibrary(__name__)
-load_server_schema = _server_schemas_lib.load_schema
-load_all_server_schemas = _server_schemas_lib.load_all_schemas
